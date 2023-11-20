69d2cf1bf7518f0afeac78bb82b4524d64ff5177
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2023-11-20 13:26:17 +0100

    commit desde eclipse

---
+++ SaludoED/src/saludos/Principal.java
@@ -0,0 +1,12 @@
+package saludos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola a todos los alumnos de Entornos de desarrollos, menos a algunos");
+		
+
+	}
+
+}
