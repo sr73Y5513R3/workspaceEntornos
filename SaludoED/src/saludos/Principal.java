0a15df2d9177ef7a7bfe02877d6cb59b8da71f37
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2023-11-21 14:21:39 +0100

    Segundo commit

--- SaludoED/src/saludos/Principal.java
+++ SaludoED/src/saludos/Principal.java
@@ -3,9 +3,9 @@ package saludos;
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hola a todos los alumnos de Entornos de desarrollos, menos a algunos");
-		
+		System.out.println("-".repeat(69));
+		System.out.println("Hola a todos los alumnos de Entornos de desarrollos, menos a algunos.");
+		System.out.println("-".repeat(69));
 
 	}
 
